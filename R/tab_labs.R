c705412af3e83716689df29837d63f5b605ee263
Urs Wilke <urs.wilke@gmail.com>
2019-08-08 16:33:12 +0200

    add .data to filter to prevent R CMD CHECK note

--- R/tab_labs.R
+++ R/tab_labs.R
@@ -1,12 +1,13 @@
 #' Create a variable & value label dataframe
 #'
 #' @param df Labelled dataframe, e.g., resulting of the read_sav / read_dta functions  of the package haven.
+#' @param rm_non_labelled Logical indicating whether the result should contain non-labelled variables (neither variable nor value labels).
 #' @return Dataframe consisting of 4 columns \code{var}, \code{varlab}, \code{val}  and \code{vallab}, showing all variable & value labels in \code{df}.
 #' @export
 #' @importFrom assertthat assert_that not_empty is.string
-#' @importFrom dplyr full_join
+#' @importFrom dplyr full_join filter
 #' @importFrom tibble tibble
-#' @importFrom rlang :=
+#' @importFrom rlang := .data
 #' @examples
 #' # load spss data
 #' path <- system.file("examples", "iris.sav", package = "haven")
@@ -19,11 +20,11 @@ tab_labs <- labs <- function(df, rm_non_labelled = FALSE){
 
   result <-
     tibble(var = names(df)) %>%
-    full_join(df %>% varl(), by = "var") %>%
-    full_join(df %>% vall(), by = "var")
+    full_join(df %>% tab_varlabs(), by = "var") %>%
+    full_join(df %>% tab_vallabs(), by = "var")
   if (rm_non_labelled == TRUE){
     result <-
-      result %>% filter(!(is.na(varlab) & is.na(val)))
+      result %>% filter(!(is.na(.data$varlab) & is.na(.data$val)))
   }
   result
 }
