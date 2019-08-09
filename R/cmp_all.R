6d2a83d8d8cf087d775d98ce6c8a684be1afe277
Urs Wilke <urs.wilke@gmail.com>
2019-08-09 11:01:11 +0200

    added argument col_groups specifying the ordering of the columns in the resulting dataframe

--- R/cmp_all.R
+++ R/cmp_all.R
@@ -3,11 +3,12 @@
 #' @param l List of dataframes.
 #' @param id name of the key variable in the dataframes.
 #' @param include_ids Logical denoting whether a list column of the ids should be included in the results. The ids in the list show at which values of \code{id} the variable \code{var} contains the value \code{val1}.
+#' @param col_groups String specifying the order of columns in the resulting dataframe. "index" groups columns by indices. "spec" groups columns by the specification types   c("ex", "varlab", "val", "vallab")
 #' @return Dataframe consisting of columns \code{var}, \code{val1}, \code{val2}, ...,  \code{vallab1}, \code{vallab2}, ...,  \code{df1}, \code{df2}, ..., and \code{n}, containing a comparison of the counts of variable values (and their respective value labels) of the dataframes in long format. The indices denote the dataframes in \code{l}. \code{vals_diff} & \code{vallabs_diff} are logical columns indicating if all values, variable / value labels are equal.
-#' @importFrom dplyr left_join full_join anti_join group_by_at rename_at ungroup select matches mutate_at arrange_at group_by row_number vars summarise n n_distinct distinct bind_rows
+#' @importFrom dplyr left_join full_join anti_join group_by_at rename_at ungroup select matches mutate_at arrange_at group_by row_number vars summarise n n_distinct distinct bind_rows everything
 #' @importFrom purrr map imap reduce walk set_names map2 map_dfr map2_lgl map_int
 #' @importFrom assertthat assert_that not_empty is.string
-#' @importFrom rlang .data
+#' @importFrom rlang .data parse_exprs
 #' @importFrom readr parse_number
 #' @export
 #' @examples
@@ -51,8 +52,9 @@
 #' # Show where either values or value labels differ:
 #' cmp %>% dplyr::filter(val_diff | vallab_diff)
 
-cmp_all <- function(l, id = "id", include_ids = FALSE){
+cmp_all <- function(l, id = "id", include_ids = FALSE, col_groups = c("index", "spec")){
   l <- unname(l)
+  col_groups <- match.arg(col_groups)
 
   df_cnt <- compare_counts(l, id, include_ids)
 
@@ -74,26 +76,30 @@ cmp_all <- function(l, id = "id", include_ids = FALSE){
     reduce(left_join, .init = df_all_vallabs)
 
 
-
-  suppressWarnings(
-    df_all <-
-      df_all %>%
-      select(.data$var, n, names(df_all) %>% parse_number() %>% order()) %>%
-      mutate(var = factor(.data$var, levels = l %>% map( ~ names(.x[-1])) %>% unlist() %>% unique())) %>%
-      arrange(.data$var))
-
   # create the logical columns indicating differences:
   col_specs <- c("ex", "varlab", "val", "vallab")
   # The reduce command creates logical columns in the dataframe for each element
   # in col_spec, indicating if there are differences in the rows in df_cnt,
   # e.g., for the element "val" in col_specs, it creates:
   # df_cnt %>% mutate(val_diff = select(df_cnt, matches("^val\\d+$")) %>% t %>% as.data.frame() %>% map_int(n_distinct) > 1)
-  reduce(col_specs, cols_differ, .init = df_all) %>%
+  df_all <-
+    reduce(col_specs, cols_differ, .init = df_all) %>%
     group_by(.data$var) %>%
     mutate(any_diff = .data$ex_diff |
              .data$val_diff | .data$varlab_diff | .data$vallab_diff) %>%
     ungroup()
 
+  if (col_groups == "index") {
+    match_exprs <- parse_exprs(paste0("matches('", 1:length(l), "')"))
+  }
+  else if (col_groups == "spec") {
+    match_exprs <- parse_exprs(paste0("matches('", col_specs, "(\\\\d+|_diff)$')"))
+  }
+  df_all %>%
+    select(.data$var,
+           n,
+           !!!match_exprs,
+           everything())
 
 }
 
